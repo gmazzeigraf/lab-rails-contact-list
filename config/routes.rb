977fa9b039b7a38db32bcbde083bb17808c1aeb3
guilherme.graf <guilherme.graf@posherapp.com>
2019-11-25 18:07:08 -0300

    Cadastro de usuarios e contatos

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root to: "static_pages#index"
 
   get 'contato', to: "static_pages#contato"
 
   get 'sobre', to: "static_pages#sobre"
+
+  resources :contacts
+
+  resources :users, only: [:new, :create]
 
 end
