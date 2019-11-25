2822e0a040b8ea91acdcf995c57e8bbf193e0078
guilherme.graf <guilherme.graf@posherapp.com>
2019-11-25 16:58:49 -0300

    Paginas estaticas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,12 @@ class StaticPagesController < ApplicationController
         @nome = "Guilherme"
     end
 
+    def sobre
+
+    end
+
+    def contato
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   root to: "static_pages#index"
 
+  get 'contato', to: "static_pages#contato"
+
+  get 'sobre', to: "static_pages#sobre"
+
 end
