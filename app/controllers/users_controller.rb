9b6fe6343b723849c9a35f403580edb87901bd77
guilherme.graf <guilherme.graf@posherapp.com>
2019-12-11 18:06:40 -0300

    Pagina de cadastro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+
+        if @user.save
+            flash[:sucess] = 'Usuário cadastro com sucesso'
+            redirect_to root_url
+        else
+            render 'new'
+        end
     end
+
+    private
+        def user_params
+            puts "params #{params.inspect}"
+            params.require(:user).permit(:email, :name, :password, :password_confirmation)
+        end
 end
